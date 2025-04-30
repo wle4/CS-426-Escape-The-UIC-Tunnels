100a25cf974a2030154ea85edca4052952732f3d
sharay2 <sharay2@uic.edu>
2025-04-30 01:06:19 -0500

    Connected level 2 to level 3

---
+++ Escape The UIC Tunnels/Assets/Scripts/Level2Door.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Level2Door : MonoBehaviour
+{
+    public float interactDistance = 3f;
+    public string nextSceneName = "Level3";  // Set this in Inspector
+    public TextMeshProUGUI promptText;
+
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (promptText != null) promptText.enabled = false;
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+
+        if (dist <= interactDistance)
+        {
+            if (!JumpScareDoor1.hasTriedLeftDoor)
+            {
+                if (promptText != null)
+                {
+                    promptText.text = "Maybe I should try the left door first...";
+                    promptText.enabled = true;
+                }
+
+                return; // Block interaction until left door is tried
+            }
+
+            if (promptText != null)
+            {
+                promptText.text = "Press E to exit";
+                promptText.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if(UIManager.instance != null)
+                    {
+                        UIManager.instance.LoadSceneWithFade(nextSceneName);
+                    }
+                    else { 
+                        SceneManager.LoadScene(nextSceneName); 
+                    }
+                }
+            }
+
+            
+        }
+        else
+        {
+            if (promptText != null) promptText.enabled = false;
+        }
+    }
+}
